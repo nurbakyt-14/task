9875c8f9fe15018b0ac65fbe193d28f7299226b7
nurbakyt-14 <n_tolepbergen@kbtu.kz>
2026-02-20 02:38:12 +0500

    Practice 3

---
+++ OneDrive/Desktop/Practice3/classes/class_definition.py
@@ -0,0 +1,5 @@
+class Person:
+    name = "Nurbakyt"
+
+p1 = Person()
+print(p1.name)
---
+++ OneDrive/Desktop/Practice3/inheritance/inheritance_basics.py
@@ -0,0 +1,9 @@
+class Student:
+    def introduce(self):
+        print("Student of Computer Science")
+
+class Freshman(Student):
+    pass
+
+d = Student()
+d.introduce()
