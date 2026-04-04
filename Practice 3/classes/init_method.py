9f97641540ea7bafdf4a00377e086422114c82e6
nurbakyt-14 <n_tolepbergen@kbtu.kz>
2026-04-04 19:18:48 +0500

    Fix: return deleted practices to repo

---
+++ Practice 3/classes/init_method.py
@@ -0,0 +1,6 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+p1 = Person("Nurbakyt")
+print(p1.name)
---
+++ Practice 3/lambda/lambda_with_sorted.py
@@ -0,0 +1,3 @@
+students = [("Ali",20), ("Aruzhan",18), ("Dias",22)]
+sorted_students = sorted(students, key=lambda x: x[1])
+print(sorted_students)
